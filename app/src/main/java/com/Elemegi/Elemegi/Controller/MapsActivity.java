34d87558bfcdcba332fdefbeb1497abb25eb768e
Akinizer <salazarslyterin@hotmail.com>
2019-12-26 04:22:54 +0300

    update for maps

--- app/src/main/java/com/Elemegi/Elemegi/Controller/MapsActivity.java
+++ app/src/main/java/com/Elemegi/Elemegi/Controller/MapsActivity.java
@@ -45,4 +45,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 }
